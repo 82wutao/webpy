7d5143991cdf94f87ab089f065ef303371893355
wutao <363697339@qq.com>
2020-08-04 08:08:57 +0800

    init

---
+++ conda.sh
@@ -0,0 +1,10 @@
+ conda create -n web  python=3.8
+
+ conda activate web
+conda install -n your_env_name [package]
+conda remove --name $your_env_name  $package_name
+
+ conda deactivate 
+
+ conda remove -n web --all
+ 
